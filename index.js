8c446f3fd8da968cc72dc0c3b2cc6ec508cbb418
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-19 17:20:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-/*function takeANumber(line,name){
+function takeANumber(line,name){
   line.push(name)
   let lineNum= line.length
-  var message = ('Welcome, ${name}. You are number ${lineNum} in line.')
+  var message = (`Welcome, ${name}. You are number ${lineNum} in line.``)
   return message
-}*/
-
-function takeANumber(lineArr, name) {
-  lineArr.push(name)
-  let linePosition = lineArr.length
-  var welcomeMessage = (`Welcome, ${name}. You are number ${linePosition} in line.`)
-  return welcomeMessage
 }
