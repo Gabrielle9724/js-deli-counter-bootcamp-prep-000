647ccf04eff8184a48503f649d2b916256dffc36
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-19 17:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(line,name){
   var message = (`Welcome, ${name}. You are number ${lineNum} in line.`)
   return message
 }
+
+function nowServing(katzDeliLine){
+  var next =  line.shift
+  if (next === 'string'){
+    return `Currently serving ${next}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
