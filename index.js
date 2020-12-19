9f2e6877985e137c63150dfab91765d915e5fca4
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-19 17:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line,name){
   line.push(name)
   let lineNum= line.length
-  var message = 'Welcome, ${name} . You are number ${lineNum} in line.'
+  var message = "Welcome, Ada. You are number 1 in line."
   return message
 }
